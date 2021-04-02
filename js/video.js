bc09c7afa8dcf0c27db9dc0d72075e66d41fd661
edwinmui <edwinmui@umich.edu>
2021-04-01 23:43:56 -0400

    finished features

--- js/video.js
+++ js/video.js
@@ -1,12 +1,37 @@
 var video = document.getElementById("player1");
 
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
     console.log("Good job opening the window")
 
 });
 
-document.querySelector("#play").addEventListener("click", function() {
+document.querySelector("#play").addEventListener("click", () => {
     console.log("Play Video");
-    video.play()
-    document.getElementById("volume").innerHTML = video.volume * 100 + "%"
+    video.play();
+    document.getElementById("volume").innerHTML = video.volume * 100 + "%";
 });
+
+document.querySelector("#pause").addEventListener("click", () => {
+    console.log("Pause Video");
+    video.pause();
+})
+
+document.querySelector("#slower").addEventListener("click", () => {
+    video.playbackRate = video.playbackRate * 0.95;
+    console.log(video.playbackRate);
+})
+
+document.querySelector("#faster").addEventListener("click", () => {
+    video.playbackRate = video.playbackRate * 1.05;
+    console.log(video.playbackRate);
+})
+
+document.querySelector("skip").addEventListener("click", () => {
+    console.log("original location " + video.currentTime);
+    video.currentTime += 15;
+    if (video.currentTime > 67.40) {
+        console.log("go back to beginning");
+        video.currentTime = 0;
+    }
+    console.log("new location " + video.currentTime);
+})

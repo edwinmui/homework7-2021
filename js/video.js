f8a6b4b3572cb1dac0b5c73534cfc2c5529cca78
edwinmui <edwinmui@umich.edu>
2021-04-01 23:54:44 -0400

    finish

--- js/video.js
+++ js/video.js
@@ -14,19 +14,19 @@ document.querySelector("#play").addEventListener("click", () => {
 document.querySelector("#pause").addEventListener("click", () => {
     console.log("Pause Video");
     video.pause();
-})
+});
 
 document.querySelector("#slower").addEventListener("click", () => {
     video.playbackRate = video.playbackRate * 0.95;
     console.log(video.playbackRate);
-})
+});
 
 document.querySelector("#faster").addEventListener("click", () => {
     video.playbackRate = video.playbackRate * 1.05;
     console.log(video.playbackRate);
-})
+});
 
-document.querySelector("skip").addEventListener("click", () => {
+document.querySelector("#skip").addEventListener("click", () => {
     console.log("original location " + video.currentTime);
     video.currentTime += 15;
     if (video.currentTime > 67.40) {
@@ -34,4 +34,35 @@ document.querySelector("skip").addEventListener("click", () => {
         video.currentTime = 0;
     }
     console.log("new location " + video.currentTime);
-})
+});
+
+document.querySelector("#mute").addEventListener("click", () => {
+    if (video.muted == false) {
+        video.muted = true;
+        console.log("muting");
+        document.querySelector("#mute").innerHTML = "Unmute";
+    } else {
+        video.muted = false;
+        console.log("unmuting");
+        document.querySelector("#mute").innerHTML = "Mute";
+    }
+});
+
+document.querySelector("#slider").addEventListener("click", () => {
+    video.volume = document.querySelector("#slider").value / 100;
+    console.log(video.volume);
+    document.getElementById("volume").innerHTML = document.querySelector("#slider").value + "%";
+    console.log(document.querySelector("#slider").value);
+});
+
+document.querySelector("#vintage").addEventListener("click", () => {
+    console.log("changing to old school class");
+    video.classList.add("oldSchool");
+    console.log("adding vintage");
+});
+
+document.querySelector("#orig").addEventListener("click", () => {
+    console.log("removing old school class");
+    video.classList.remove("oldSchool");
+    console.log("removing vintage");
+});
